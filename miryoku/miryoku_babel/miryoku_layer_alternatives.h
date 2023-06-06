841483778b8c230fa4b92ed6222eea2cb964e68f
Justin Binder <jbinder@apple.com>
2023-06-06 07:21:32 -0700

    Making L/R variants of the shift/cmd key. Tweaking tapping term.

--- miryoku/miryoku_babel/miryoku_layer_alternatives.h
+++ miryoku/miryoku_babel/miryoku_layer_alternatives.h
@@ -56,7 +56,7 @@ U_NP,              U_NP,              U_LT(U_FUN, DEL),  U_LT(U_NUM, BSPC), U_LT
 
 #define MIRYOKU_ALTERNATIVES_BASE_QWERTY_FLIP \
 &kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
-U_MTC(LGUI, A),     U_MT(LALT, S),     U_MT(LCTRL, D),    U_MTS(LSHFT, F),    &kp G,             &kp H,             U_MTS(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     U_MTC(LGUI, SQT),   \
+U_MTC(LGUI, A),     U_MT(LALT, S),     U_MTS(LCTRL, D),    U_MTS(LSHFT, F),    &kp G,             &kp H,             U_MTS(LSHFT, J),    U_MTSR(LCTRL, K),    U_MT(LALT, L),     U_MTC(LGUI, SQT),   \
 U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
 U_NP,              U_NP,              U_LT(U_FUN, DEL),  U_LT(U_NUM, BSPC), U_LT(U_SYM, RET),  U_LT(U_MOUSE, TAB),U_LT(U_NAV, SPACE),U_LT(U_MEDIA, ESC),U_NP,              U_NP
 
@@ -116,7 +116,7 @@ U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT
 
 #define MIRYOKU_ALTERNATIVES_BASE_QWERTY \
 &kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
-U_MTC(LGUI, A),     U_MT(LALT, S),     U_MT(LCTRL, D),    U_MTS(LSHFT, F),    &kp G,             &kp H,             U_MTS(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     U_MTC(LGUI, SQT),   \
+U_MTC(LGUI, A),     U_MT(LALT, S),     U_MTS(LCTRL, D),    U_MTS(LSHFT, F),    &kp G,             &kp H,             U_MTSR(LSHFT, J),    U_MTSR(LCTRL, K),    U_MT(LALT, L),     U_MTC(LGUI, SQT),   \
 U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
 U_NP,              U_NP,              U_LT(U_MEDIA, TAB),U_LT(U_NAV, BSPC),U_LT(U_MOUSE, DEL),U_LT(U_SYM, RET),  U_LT(U_NUM, SPACE), U_LT(U_FUN, ESC),  U_NP,              U_NP
 
--- miryoku/miryoku_behaviors.h
+++ miryoku/miryoku_behaviors.h
@@ -5,5 +5,6 @@
 
 #define U_MT(MOD, TAP) &u_mt MOD TAP
 #define U_MTS(MOD, TAP) &u_mts MOD TAP
+#define U_MTSR(MOD, TAP) &u_mtsr MOD TAP
 #define U_MTC(MOD, TAP) &u_mtc MOD TAP
 #define U_LT(LAYER, TAP) &u_lt LAYER TAP
